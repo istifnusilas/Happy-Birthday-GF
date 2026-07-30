534b92c67a47fe248ec7fd27adec455e28204553
istifnusilas <istifnusilas@gmail.com>
2026-07-30 16:55:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Happy Birthday, my love! ❤️ Today is all about you. I hope this little surprise makes you smile. Keep scrolling because I have something special waiting for you. 💕";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
